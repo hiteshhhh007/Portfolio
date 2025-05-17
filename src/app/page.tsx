160cec0f37e7b5cc559aa67110b2884bf4af0e3d
Hitesh <hiteshkrishna.kv2022@vitstudent.ac.in>
2025-05-17 16:36:54 +0530

    fix eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -150,7 +150,7 @@ export default function Page() {
                   Check out my latest work
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  I've worked on a variety of projects, from simple
+                  I&apos;ve worked on a variety of projects, from simple
                   websites to complex web applications. Here are a few of my
                   favorites.
                 </p>
@@ -239,7 +239,7 @@ export default function Page() {
                 >
                   with a direct question on twitter
                 </Link>{" "}
-                and I'll respond whenever I can. I will ignore all
+                and I&apos;ll respond whenever I can. I will ignore all
                 soliciting.
               </p>
             </div>
